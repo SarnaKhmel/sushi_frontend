f63cc0aa159d6f63bb73275ffec85796b5de4c26
sarnakhmel <OlSa@olsa.local>
2023-06-25 11:42:49 +0300

    ssl

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// const instance = axios.create({
-//   baseURL: "http://localhost:1234",
-// });
-
 const instance = axios.create({
   baseURL: "/backend",
 });
--- src/Utils/baseUrl.js
+++ src/Utils/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://164.92.250.198/backend";
+export const baseUrl = "https://164.92.250.198/backend";
 
 //export const baseUrl = "http://localhost:1234";
