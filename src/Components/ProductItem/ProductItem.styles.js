95f8d74e0d5ac4204af4c56d9de4e0f05ad19a16
sarnakhmel <OlSa@olsa.local>
2023-07-01 13:06:15 +0300

    bg

--- src/Components/ProductItem/ProductItem.styles.js
+++ src/Components/ProductItem/ProductItem.styles.js
@@ -6,7 +6,7 @@ export const ProductItemBlock = styled.div`
   justify-content: center;
 
   height: 60vh;
-  margin-top: 180px;
+  margin-top: 100px;
 
   @media (max-width: 768px) {
     margin-top: 180px;
