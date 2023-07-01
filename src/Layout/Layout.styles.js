f4c728575fda57ff9490a9cf6a1a6aa9d0d0302b
sarnakhmel <OlSa@olsa.local>
2023-07-01 13:39:09 +0300

    bg

--- src/Layout/Layout.styles.js
+++ src/Layout/Layout.styles.js
@@ -7,6 +7,7 @@ export const LayoutBlock = styled.div`
   min-height: 80vh;
   height: 100vh - 334px;
   background-repeat: repeat;
+  background-size: cover;
   overflow: auto;
   overflow-y: scroll;
   z-index: 1;
