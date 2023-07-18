719b205a15872ca1e1d593448102b3f82626a151
sarnakhmel <OlSa@olsa.local>
2023-07-18 19:58:01 +0300

    Added new edits. selector

--- src/Components/Select/Select.styles.js
+++ src/Components/Select/Select.styles.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const SelectBox = styled.div`
   width: 260px;
   margin-left: 5vw;
+  background: linear-gradient(
+    145.99deg,
+    rgba(22, 21, 21, 0.9) -0.09%,
+    rgba(46, 46, 46, 0.9) 98.66%
+  );
 `;
