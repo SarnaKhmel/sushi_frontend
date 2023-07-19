c35a0d5702cf154afe2f6152519c2bc41b916b75
sarnakhmel <OlSa@olsa.local>
2023-07-19 16:34:34 +0300

    Added new edits. selector bg

--- src/Components/Select/Select.jsx
+++ src/Components/Select/Select.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Select from "react-select";
-import { SelectBox } from "./Select.styles";
+import styled from "styled-components";
 
 const SelectBlock = ({ selectOptions, handleSelectedOption }) => {
   const handleChange = (selectedOption) => {
@@ -8,15 +8,84 @@ const SelectBlock = ({ selectOptions, handleSelectedOption }) => {
   };
 
   return (
-    <SelectBox>
-      <Select
+    <CustomSelectBox>
+      <CustomSelect
         options={selectOptions}
         className="react-select-container"
         classNamePrefix="react-select"
         onChange={handleChange}
       />
-    </SelectBox>
+    </CustomSelectBox>
   );
 };
+
+const CustomSelectBox = styled.div`
+  width: 260px;
+  margin-left: 5vw;
+`;
+
+const CustomSelect = styled(Select)`
+  /* Ваші стилі для Select */
+  .react-select__control {
+    border-radius: 15px;
+    background-color: transparent;
+  }
+
+  .react-select__single-value {
+    color: white;
+    font-size: 14px;
+  }
+
+  .react-select__value-container {
+    .react-select__menu {
+      background-color: transparent;
+      color: white;
+    }
+
+    .react-select__menu-list {
+      background-color: transparent;
+      color: white;
+
+      .react-select__option {
+        background-color: transparent;
+        color: white;
+      }
+
+      .react-select__option:hover {
+        color: #ff4700;
+      }
+
+      .react-select__option:focus {
+        color: #ff4700;
+      }
+    }
+  }
+
+  .react-select__menu {
+    border-radius: 5px;
+    padding: 5px;
+    background: linear-gradient(
+      145.99deg,
+      rgba(22, 21, 21, 0.9) -0.09%,
+      rgba(46, 46, 46, 0.9) 98.66%
+    );
+
+    .react-select__menu-list {
+      color: white;
+
+      .react-select__option {
+        margin-top: 2px;
+        border-radius: 5px;
+        background: linear-gradient(
+          145.99deg,
+          rgba(22, 21, 21, 0.9) -0.09%,
+          rgba(46, 46, 46, 0.9) 98.66%
+        );
+
+        color: white;
+      }
+    }
+  }
+`;
 
 export default SelectBlock;
