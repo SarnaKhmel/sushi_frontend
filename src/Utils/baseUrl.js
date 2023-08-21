f3441e3ef3d0cae03bd58dab4e1856d130297c9a
sarnakhmel <OlSa@olsa.local>
2023-08-22 00:24:27 +0300

    fin

--- src/Utils/baseUrl.js
+++ src/Utils/baseUrl.js
@@ -1,3 +1,5 @@
-export const baseUrl = "https://www.sushiwithlove.pp.ua/backend";
+export const baseUrl = "https://sushizlyubovyu.com.ua/backend";
+
+// export const baseUrl = "https://www.sushiwithlove.pp.ua/backend";
 
 //export const baseUrl = "http://localhost:1234";
