1524176b2f4397468e817abd13cfc4685d8072fa
sarnakhmel <OlSa@olsa.local>
2023-08-22 03:42:40 +0300

    fin

--- src/App.js
+++ src/App.js
@@ -2,108 +2,109 @@ import "./App.css";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
 import ProductPage from "./Pages/ProductPage";
-import ErrorPage from "./Pages/ErrorPage";
-import { Routes, Route } from "react-router";
+// import ErrorPage from "./Pages/ErrorPage";
+// import { Routes, Route } from "react-router";
 
-import RegisterPage from "./Admin/Pages/RegisterPage";
-import LoginAdmin from "./Admin/Login/LoginAdmin";
-// import OrderPageA from "./Admin/Pages/OrderPageA";
-import HomeAdminPage from "./Admin/Pages/HomeAdminPage";
-import AdminProductsPage from "./Admin/Pages/AdminProductsPage";
-import AdminUserPage from "./Admin/Pages/AdminUserPage";
-import AdminProductEditPage from "./Admin/Pages/AdminProductEditPage";
-import AdminPostPage from "./Admin/Pages/AdminPostPage";
-import AdminOrderPage from "./Admin/Pages/AdminOrderPage";
+// import RegisterPage from "./Admin/Pages/RegisterPage";
+// import LoginAdmin from "./Admin/Login/LoginAdmin";
+// // import OrderPageA from "./Admin/Pages/OrderPageA";
+// import HomeAdminPage from "./Admin/Pages/HomeAdminPage";
+// import AdminProductsPage from "./Admin/Pages/AdminProductsPage";
+// import AdminUserPage from "./Admin/Pages/AdminUserPage";
+// import AdminProductEditPage from "./Admin/Pages/AdminProductEditPage";
+// import AdminPostPage from "./Admin/Pages/AdminPostPage";
+// import AdminOrderPage from "./Admin/Pages/AdminOrderPage";
 
-import PostPage from "./Pages/PostPage";
-import DeliveryPage from "./Pages/DeliveryPage";
-import PoliticsPage from "./Pages/PoliticsPage";
-import OrderPage from "./Pages/OrderPage";
-import OfertaPage from "./Pages/OfertaPage";
-import { useContext, useEffect, useLayoutEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+// import PostPage from "./Pages/PostPage";
+// import DeliveryPage from "./Pages/DeliveryPage";
+// import PoliticsPage from "./Pages/PoliticsPage";
+// import OrderPage from "./Pages/OrderPage";
+// import OfertaPage from "./Pages/OfertaPage";
+// import { useContext, useEffect, useLayoutEffect, useState } from "react";
+// import { useLocation } from "react-router-dom";
 
-import { yScrollContext } from "./Utils/yScroll";
-import { history } from "./Utils/CustomRouter";
+// import { yScrollContext } from "./Utils/yScroll";
+// import { history } from "./Utils/CustomRouter";
 
 function App() {
-  const { yScroll, setYscroll } = useContext(yScrollContext);
-  const location = useLocation();
-  const [currHist, setCurrHist] = useState({
-    action: "",
-    location: {},
-    currentKey: "",
-  });
+  // const { yScroll, setYscroll } = useContext(yScrollContext);
+  // const location = useLocation();
+  // const [currHist, setCurrHist] = useState({
+  //   action: "",
+  //   location: {},
+  //   currentKey: "",
+  // });
 
-  useEffect(() => {
-    sessionStorage.setItem("key", location.key);
-  }, [location]);
+  // useEffect(() => {
+  //   sessionStorage.setItem("key", location.key);
+  // }, [location]);
 
-  useState(() => {
-    const scrolled = () => setYscroll((prev) => window.scrollY);
-    window.addEventListener("scroll", scrolled);
-    return () => {
-      window.removeEventListener("scroll", scrolled);
-    };
-  }, []);
+  // useState(() => {
+  //   const scrolled = () => setYscroll((prev) => window.scrollY);
+  //   window.addEventListener("scroll", scrolled);
+  //   return () => {
+  //     window.removeEventListener("scroll", scrolled);
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    return () => {
-      sessionStorage.setItem("yvalue", JSON.stringify(yScroll));
-    };
-  }, [yScroll]);
+  // useEffect(() => {
+  //   return () => {
+  //     sessionStorage.setItem("yvalue", JSON.stringify(yScroll));
+  //   };
+  // }, [yScroll]);
 
-  useEffect(() => {
-    return history.listen(({ location, action }) => {
-      if (action === "POP") {
-        const fwdKey = sessionStorage.getItem("key");
-        const yscroll = sessionStorage.getItem("yvalue");
-        location = {
-          ...location,
-          state: {
-            forward: {
-              key: fwdKey,
-              yscroll: yscroll,
-            },
-            ...location.state,
-          },
-        };
-      }
-      setCurrHist({
-        ...currHist,
-        action: action,
-        location: {
-          ...location,
-        },
-        currentKey: location.key,
-      });
-      console.log(action, location.pathname, location.state, location.key);
-    });
-  }, []);
+  // useEffect(() => {
+  //   return history.listen(({ location, action }) => {
+  //     if (action === "POP") {
+  //       const fwdKey = sessionStorage.getItem("key");
+  //       const yscroll = sessionStorage.getItem("yvalue");
+  //       location = {
+  //         ...location,
+  //         state: {
+  //           forward: {
+  //             key: fwdKey,
+  //             yscroll: yscroll,
+  //           },
+  //           ...location.state,
+  //         },
+  //       };
+  //     }
+  //     setCurrHist({
+  //       ...currHist,
+  //       action: action,
+  //       location: {
+  //         ...location,
+  //       },
+  //       currentKey: location.key,
+  //     });
+  //     // console.log(action, location.pathname, location.state, location.key);
+  //   });
+  // }, []);
 
-  useLayoutEffect(() => {
-    const { action, location, currentKey } = currHist;
-    if (action && action === "POP") {
-      if (location.state.hasOwnProperty("forward")) {
-        if (currentKey === location.state.forward.key) {
-          console.log(`forward ${location.state.forward.yscroll}`);
-        }
-      } else if (location.state.hasOwnProperty("backward")) {
-        if (currentKey === location.state.backward.key) {
-          console.log(`backward ${location.state.backward.yscroll}`);
-          window.scrollTo(0, parseFloat(location.state.backward.yscroll));
-        }
-      } else {
-        window.scrollTo(0, 0);
-      }
-    } else {
-      window.scrollTo(0, 0);
-    }
-    console.log(location);
-  }, [currHist]);
+  // useLayoutEffect(() => {
+  //   const { action, location, currentKey } = currHist;
+  //   if (action && action === "POP") {
+  //     if (location.state.hasOwnProperty("forward")) {
+  //       if (currentKey === location.state.forward.key) {
+  //         // console.log(`forward ${location.state.forward.yscroll}`);
+  //       }
+  //     } else if (location.state.hasOwnProperty("backward")) {
+  //       if (currentKey === location.state.backward.key) {
+  //         // console.log(`backward ${location.state.backward.yscroll}`);
+  //         window.scrollTo(0, parseFloat(location.state.backward.yscroll));
+  //       }
+  //     } else {
+  //       window.scrollTo(0, 0);
+  //     }
+  //   } else {
+  //     window.scrollTo(0, 0);
+  //   }
+  //   // console.log(location);
+  // }, [currHist]);
   return (
-    <>
-      <Routes>
+    <div>
+      hi
+      {/* <Routes>
         <Route path="/" element={<HomePage />} />
 
         <Route path="/about" element={<AboutPage />} />
@@ -117,23 +118,19 @@ function App() {
         <Route path="/post/:id" element={<PostPage />} />
 
         {/* Admins routes */}
-
-        {/* <Route path="/admin/register" element={<RegisterPage />} /> */}
-        <Route path="/admin/login" element={<LoginAdmin />} />
-
-        <Route path="/admin/home" element={<HomeAdminPage />} />
-        {/* <Route path="/admin/admins" element={<RegisterPage />} /> */}
-        <Route path="/admin/products" element={<AdminProductsPage />} />
-        <Route path="/admin/products/:id" element={<AdminProductEditPage />} />
-        <Route path="/admin/posts" element={<AdminPostPage />} />
-        <Route path="/admin/orders" element={<AdminOrderPage />} />
-
-        {/* <Route path="/admin/statistics" element={<RegisterPage />} /> */}
-        <Route path="/admin/me" element={<AdminUserPage />} />
-
-        <Route path="*" element={<HomePage />} />
-      </Routes>
-    </>
+      {/* <Route path="/admin/register" element={<RegisterPage />} /> */}
+      {/* <Route path="/admin/login" element={<LoginAdmin />} /> */}
+      {/* <Route path="/admin/home" element={<HomeAdminPage />} /> */}
+      {/* <Route path="/admin/admins" element={<RegisterPage />} /> */}
+      {/* <Route path="/admin/products" element={<AdminProductsPage />} /> */}
+      {/* <Route path="/admin/products/:id" element={<AdminProductEditPage />} /> */}
+      {/* <Route path="/admin/posts" element={<AdminPostPage />} /> */}
+      {/* <Route path="/admin/orders" element={<AdminOrderPage />} /> */}
+      {/* <Route path="/admin/statistics" element={<RegisterPage />} /> */}
+      {/* <Route path="/admin/me" element={<AdminUserPage />} /> */}
+      {/* <Route path="*" element={<HomePage />} /> */}
+      {/* </Routes> */}
+    </div>
   );
 }
 
