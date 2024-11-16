bf59655893a41bd44e722a2c89f52a15f16112f9
sarnakhmel <olsarnat@gmail.com>
2024-11-16 02:05:19 +0200

    Update Select.jsx

--- src/Admin/Select/Select.jsx
+++ src/Admin/Select/Select.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import Select from "react-select";
-import { SelectBox } from "./Select.styles";
+import { TextField, MenuItem } from "@mui/material";
+import { styled } from "@mui/system";
+
+const SelectBox = styled("div")({
+    margin: "16px 0", // Стилі для контейнера (можна змінити залежно від потреб)
+});
 
 const SelectBlock = ({ selectOptions, handleSelectedOption }) => {
-  const handleChange = (selectedOption) => {
-    handleSelectedOption(selectedOption.value);
-  };
+    const handleChange = (event) => {
+        handleSelectedOption(event.target.value);
+    };
 
-  return (
-    <SelectBox>
-      <Select
-        options={selectOptions}
-        className="react-select-container"
-        classNamePrefix="react-select"
-        onChange={handleChange}
-      />
-    </SelectBox>
-  );
+    return (
+        <SelectBox>
+            <TextField
+                select
+                fullWidth
+                variant="outlined"
+                label="Фільтрація"
+                onChange={handleChange}
+            >
+                {selectOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+        </SelectBox>
+    );
 };
 
 export default SelectBlock;
