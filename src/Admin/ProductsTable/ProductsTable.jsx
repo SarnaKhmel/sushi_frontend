d486203f883041ac7cb790506c89e9f80a185a0e
sarnakhmel <olsarnat@gmail.com>
2024-11-16 02:05:04 +0200

    Update ProductsTable.jsx

--- src/Admin/ProductsTable/ProductsTable.jsx
+++ src/Admin/ProductsTable/ProductsTable.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import styled from "styled-components";
-import { baseUrl } from "../../Utils/baseUrl";
-import {
-  fetchRemoveProduct,
-  fetchRemoveProductImage,
-} from "../../Redux/slices/products";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-
-import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
-import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import { baseUrl } from "../../Utils/baseUrl";
+import { fetchRemoveProduct, fetchRemoveProductImage } from "../../Redux/slices/products";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Button,
+  Paper,
+  Box,
+} from "@mui/material";
 
 const ProductsTable = ({ products }) => {
   const dispatch = useDispatch();
 
   const onHandleDeleteProduct = (id, url) => {
     const imageName = url.split("/").pop();
-    const confirmed = window.confirm(
-      `Are you sure you want to delete this product?`
-    );
+    const confirmed = window.confirm("Ви впевнені, що хочете видалити цей продукт?\n");
     if (confirmed) {
       const result = dispatch(fetchRemoveProduct(id));
       dispatch(fetchRemoveProductImage(imageName));
@@ -27,73 +29,62 @@ const ProductsTable = ({ products }) => {
   };
 
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Td>Номер</Td>
-          <Td>Зображення</Td>
-          <Td>Назва</Td>
-          <Td>Вага</Td>
-          <Td>Ціна</Td>
-          <Td>Редагувати</Td>
-          <Td>Видалити</Td>
-        </Tr>
-      </Thead>
-
-      <Tbody>
-        {products.length === 0 ? (
-          <Tr>
-            <Th>Товарів даної категорії немає</Th>
-          </Tr>
-        ) : (
-          <>
-            {products &&
-              products.map((product, index) => (
-                <Tr key={index}>
-                  <Td>{index + 1}.</Td>
-                  <Td>
-                    <Image
-                      src={`${baseUrl}${product.imageUrl}`}
-                      alt={product.name}
-                      loading="lazy"
-                    />
-                  </Td>
-                  <Td>{product.name}</Td>
-                  <Td>{product.weight}</Td>
-                  <Td>{product.price}</Td>
-                  <Td>
-                    <Button>
-                      <Link to={`/admin/products/${product._id}`}>
-                        Редагувати
-                      </Link>
-                    </Button>
-                  </Td>
-                  <Td>
-                    <Button
-                      onClick={() => {
-                        onHandleDeleteProduct(product._id, product.imageUrl);
-                      }}>
-                      Видалити
-                    </Button>
-                  </Td>
-                </Tr>
-              ))}
-          </>
-        )}
-      </Tbody>
-    </Table>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Номер</TableCell>
+              <TableCell>Зображення</TableCell>
+              <TableCell>Назва</TableCell>
+              <TableCell>Вага</TableCell>
+              <TableCell>Ціна</TableCell>
+              <TableCell>Редагувати</TableCell>
+              <TableCell>Видалити</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={7} align="center">
+                    Товарів даної категорії немає
+                  </TableCell>
+                </TableRow>
+            ) : (
+                products.map((product, index) => (
+                    <TableRow key={product._id}>
+                      <TableCell>{index + 1}.</TableCell>
+                      <TableCell>
+                        <Box
+                            component="img"
+                            src={`${baseUrl}${product.imageUrl}`}
+                            alt={product.name}
+                            sx={{ height: 80, width: 80, objectFit: "cover" }}
+                        />
+                      </TableCell>
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell>{product.weight}</TableCell>
+                      <TableCell>{product.price}</TableCell>
+                      <TableCell>
+                        <Button variant="outlined" component={Link} to={`/admin/products/${product._id}`}>
+                          Редагувати
+                        </Button>
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => onHandleDeleteProduct(product._id, product.imageUrl)}
+                        >
+                          Видалити
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
   );
 };
-
-const Image = styled.img`
-  height: 80px;
-  width: 80px;
-`;
-
-const Button = styled.button`
-  &:hover {
-    color: #007bff;
-  }
-`;
 
 export default ProductsTable;
