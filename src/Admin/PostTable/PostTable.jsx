38cdbb082564a4897d2a79bbf9a955e19c4da295
sarnakhmel <olsarnat@gmail.com>
2024-11-16 02:04:23 +0200

    Update PostTable.jsx

--- src/Admin/PostTable/PostTable.jsx
+++ src/Admin/PostTable/PostTable.jsx
@@ -1,26 +1,19 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useDispatch } from "react-redux";
 import { baseUrl } from "../../Utils/baseUrl";
-import {
-  fetchRemovePost,
-  fetchRemovePostImage,
-} from "../../Redux/slices/posts";
-import { useDispatch, useSelector } from "react-redux";
-
-import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
-import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import { fetchRemovePost, fetchRemovePostImage } from "../../Redux/slices/posts";
+import { Table, TableHead, TableBody, TableRow, TableCell, Button, IconButton, Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const PostTable = ({ posts }) => {
   const dispatch = useDispatch();
 
-  const onHandleDeleteProduct = (id, url) => {
+  const onHandleDeletePost = (id, url) => {
     const imageName = url.split("/").pop();
-    const confirmed = window.confirm(`Ви дійсно хочете видалити пост?`);
+    const confirmed = window.confirm("Ви дійсно хочете видалити пост?");
     if (confirmed) {
-      const result = dispatch(fetchRemovePost(id));
+      dispatch(fetchRemovePost(id));
       dispatch(fetchRemovePostImage(imageName));
-      // dispatch(fetchRemoveProduct(id));
-      console.log(result);
     }
   };
 
@@ -29,99 +22,41 @@ const PostTable = ({ posts }) => {
     setItems(posts.items);
   }, [posts]);
 
-  const handleSetOption = (type) => {};
-  console.log(items);
   return (
-    <>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Номер</Th>
-            <Th>Зображення</Th>
-            <Th>Назва</Th>
-            <Th>Текст</Th>
-            <Th>Кількість переглядів</Th>
-            <Th>Видалити</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
+      <Table sx={{ width: "100%", mt: 2 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell><Typography variant="h6">Номер</Typography></TableCell>
+            <TableCell><Typography variant="h6">Зображення</Typography></TableCell>
+            <TableCell><Typography variant="h6">Назва</Typography></TableCell>
+            <TableCell><Typography variant="h6">Текст</Typography></TableCell>
+            <TableCell><Typography variant="h6">Кількість переглядів</Typography></TableCell>
+            <TableCell><Typography variant="h6">Видалити</Typography></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {items.map((item, index) => (
-            <Tr key={index}>
-              <Td>{index + 1}.</Td>
-              <Td>
-                <Image src={`${baseUrl}${item.imageUrl}`} alt={item.title} />
-              </Td>
-              {/* {baseUrl}${item.imageUrl} */}
-              <Td>{item.title}</Td>
-              <Td>{item.text}</Td>
-              <Td>{item.viewsCount}</Td>
-              <Td>
-                <Button
-                  onClick={() => {
-                    onHandleDeleteProduct(item._id, item.imageUrl);
-                  }}>
-                  Видалити
-                </Button>
-              </Td>
-            </Tr>
+              <TableRow key={item._id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>
+                  <img src={`${baseUrl}${item.imageUrl}`} alt={item.title} style={{ height: 80, width: 80 }} />
+                </TableCell>
+                <TableCell>{item.title}</TableCell>
+                <TableCell>{item.text}</TableCell>
+                <TableCell>{item.viewsCount}</TableCell>
+                <TableCell>
+                  <IconButton
+                      color="error"
+                      onClick={() => onHandleDeletePost(item._id, item.imageUrl)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
           ))}
-        </Tbody>
+        </TableBody>
       </Table>
-    </>
   );
 };
-
-// const Table = styled.table`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   background: lightGray;
-
-//   /* Mobile styles */
-//   @media (max-width: 768px) {
-//     width: 100%;
-//   }
-// `;
-
-const TableHeader = styled.thead`
-  margin: 20px 0px;
-`;
-
-const Image = styled.img`
-  height: 80px;
-  width: 80px;
-`;
-
-// const Tr = styled.tr`
-//   display: flex;
-//   align-items: center;
-//   margin-bottom: 10px;
-// `;
-
-// const TrHead = styled.tr`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const Th = styled.th`
-//   border: 1px solid violet;
-//   margin: 20px;
-//   &:hover {
-//     color: #007bff;
-//   }
-// `;
-
-// const Td = styled.td`
-//   width: 150px;
-//   overflow-x: scroll;
-//   height: 80px;
-// `;
-
-const Button = styled.button`
-  &:hover {
-    color: #007bff;
-  }
-`;
 
 export default PostTable;
