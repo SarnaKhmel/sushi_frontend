51f6200e775d52db02647e189079b1fa2b8e91b1
sarnakhmel <olsarnat@gmail.com>
2024-11-16 02:04:54 +0200

    Update Products.jsx

--- src/Admin/Products/Products.jsx
+++ src/Admin/Products/Products.jsx
@@ -1,87 +1,83 @@
-import styled, { css } from "styled-components";
 import { useState, useEffect } from "react";
+import {
+  Box,
+  Container,
+  Paper,
+  Typography,
+  Tabs,
+  Tab,
+  Button,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  Chip,
+  useTheme,
+  styled
+} from "@mui/material";
 import AddProduct from "../AddProduct/AddProduct";
 import adminMenuOptions from "../../testData/adminMenuOption.json";
 import ProductsTable from "../ProductsTable/ProductsTable";
 import Exel from "../Exel/Exel";
-import { useDispatch } from "react-redux";
 import SelectBlock from "../Select/Select";
 import selectOptions from "../../testData/selectOptions.json";
 
-import { Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
-import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
-
 const Products = ({ products }) => {
-  const [activeBlocks, setActiveBlocks] = useState([false, false, false]);
+  const theme = useTheme();
+  const [activeTab, setActiveTab] = useState(0);
   const [filter, setFilter] = useState("all");
   const [sort, setSort] = useState("");
-  const [underlined, setUnderlined] = useState(0);
+  const [selectedFilter, setSelectedFilter] = useState(0);
   const [exelName, setExelName] = useState("Всі");
+  const [filteredProducts, setFilteredProducts] = useState(products);
 
-  const toggleBlock = (blockNumber) => {
-    setActiveBlocks((prevActiveBlocks) => {
-      const newActiveBlocks = [...prevActiveBlocks];
-      newActiveBlocks[blockNumber] = !prevActiveBlocks[blockNumber];
-      return newActiveBlocks;
-    });
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
   };
 
-  const handlerUnderlined = (index, option) => {
-    setUnderlined(index);
+  const handleFilterClick = (index, option) => {
+    setSelectedFilter(index);
     setFilter(option.type);
     setExelName(option.name);
   };
-
-  const [filteredProducts, setFilteredProducts] = useState(products);
 
   useEffect(() => {
     if (filter === "all") {
       setFilteredProducts(products);
     } else if (filter === "sale") {
-      const filteredItems = products.filter((item) => item.sale === true);
-      setFilteredProducts(filteredItems);
+      setFilteredProducts(products.filter((item) => item.sale === true));
     } else if (filter === "week_sale") {
-      const filteredItems = products.filter((item) => item.week_sale === true);
-      setFilteredProducts(filteredItems);
+      setFilteredProducts(products.filter((item) => item.week_sale === true));
     } else if (filter !== "") {
-      const filteredItems = products.filter((item) => item.type === filter);
-      setFilteredProducts(filteredItems);
+      setFilteredProducts(products.filter((item) => item.type === filter));
     } else {
       setFilteredProducts(products);
     }
-  }, [filter]);
+  }, [filter, products]);
 
   useEffect(() => {
+    const sortedProducts = [...filteredProducts];
     switch (sort) {
       case "price-up":
-        setFilteredProducts(
-          [...filteredProducts].sort((a, b) => a.price - b.price)
-        );
+        sortedProducts.sort((a, b) => a.price - b.price);
         break;
       case "price-down":
-        setFilteredProducts(
-          [...filteredProducts].sort((a, b) => b.price - a.price)
-        );
+        sortedProducts.sort((a, b) => b.price - a.price);
         break;
       case "popular-up":
-        setFilteredProducts(
-          [...filteredProducts].sort((a, b) => b.viewsCount - a.viewsCount)
-        );
+        sortedProducts.sort((a, b) => b.viewsCount - a.viewsCount);
         break;
       case "weight-up":
-        setFilteredProducts(
-          [...filteredProducts].sort((a, b) => a.weight - b.weight)
-        );
+        sortedProducts.sort((a, b) => a.weight - b.weight);
         break;
       case "weight-down":
-        setFilteredProducts(
-          [...filteredProducts].sort((a, b) => b.weight - a.weight)
-        );
+        sortedProducts.sort((a, b) => b.weight - a.weight);
         break;
       default:
-        setFilteredProducts(filteredProducts);
         break;
     }
+    setFilteredProducts(sortedProducts);
   }, [sort]);
 
   const handleSelectedOption = (value) => {
@@ -89,133 +85,128 @@ const Products = ({ products }) => {
   };
 
   return (
-    <Container>
-      <LabelBlock>
-        <Label onClick={() => toggleBlock(0)}>Додати товар</Label>
-        <Label onClick={() => toggleBlock(1)}>Всі товари</Label>
-      </LabelBlock>
+        <Paper sx={{ mt: 2, mb: 4 }}>
+          <Tabs
+              value={activeTab}
+              onChange={handleTabChange}
+              variant="fullWidth"
+              sx={{ borderBottom: 1, borderColor: 'divider' }}
+          >
+            <Tab
+                label="Додати товар"
+                sx={{
+                  fontSize: { xs: '0.875rem', sm: '1.125rem' },
+                  '&:hover': { color: 'primary.main' }
+                }}
+            />
+            <Tab
+                label="Всі товари"
+                sx={{
+                  fontSize: { xs: '0.875rem', sm: '1.125rem' },
+                  '&:hover': { color: 'primary.main' }
+                }}
+            />
+          </Tabs>
 
-      {activeBlocks[0] && (
-        <Block $active={activeBlocks[0]}>
-          <AddProduct />
-        </Block>
-      )}
+          {/* Add Product Tab Panel */}
+          <TabPanel value={activeTab} index={0}>
+            <AddProduct />
+          </TabPanel>
 
-      {activeBlocks[1] && (
-        <Block $active={activeBlocks[1]}>
-          <Exel products={filteredProducts} name={exelName} />
-          <SelectBlock
-            selectOptions={selectOptions}
-            handleSelectedOption={handleSelectedOption}
-          />
-          <Header>
-            {adminMenuOptions.map((option, index) => (
-              <Link
-                key={index}
-                isUnderlined={index === underlined}
-                onClick={() => {
-                  handlerUnderlined(index, option);
-                }}>
-                {option.name}
-              </Link>
-            ))}
-          </Header>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>Продукти</Th>
-              </Tr>
-            </Thead>
+          {/* All Products Tab Panel */}
+          <TabPanel value={activeTab} index={1}>
+            <Box sx={{ width: '100%', p: 2 }}>
+              <Box sx={{ mb: 3 }}>
+                <Exel products={filteredProducts} name={exelName} />
+              </Box>
 
-            <ProductsTable products={filteredProducts} />
-          </Table>
-        </Block>
-      )}
-    </Container>
+              <Box sx={{ mb: 3 }}>
+                <SelectBlock
+                    selectOptions={selectOptions}
+                    handleSelectedOption={handleSelectedOption}
+                />
+              </Box>
+
+              {/* Filter Chips */}
+              <Box
+                  sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: 2,
+                    mb: 3,
+                    px: 2,
+                    overflowX: 'auto',
+                    '&::-webkit-scrollbar': {
+                      height: '8px',
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                      backgroundColor: 'rgba(0,0,0,0.2)',
+                      borderRadius: '4px',
+                    }
+                  }}
+              >
+                {adminMenuOptions.map((option, index) => (
+                    <StyledChip
+                        key={index}
+                        label={option.name}
+                        onClick={() => handleFilterClick(index, option)}
+                        color={selectedFilter === index ? "primary" : "default"}
+                        variant={selectedFilter === index ? "filled" : "outlined"}
+                    />
+                ))}
+              </Box>
+
+              {/* Products Table */}
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="h6">Продукти</Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <ProductsTable products={filteredProducts} />
+                </Table>
+              </TableContainer>
+            </Box>
+          </TabPanel>
+        </Paper>
   );
 };
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: start;
-  background: grey;
-  min-height: 100vh;
-`;
+// Styled Components
+const StyledChip = styled(Chip)(({ theme }) => ({
+  fontSize: '1rem',
+  padding: theme.spacing(1),
+  '&:hover': {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText,
+  },
+  '&.MuiChip-clickable': {
+    cursor: 'pointer',
+  },
+}));
 
-const Block = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: lightGray;
-  min-height: 50vh;
-  width: 100vw;
-  border-bottom: 1px solid black;
-  display: none;
-  opacity: 0;
-  transition: opacity 3s ease;
-
-  ${(props) =>
-    props.$active &&
-    css`
-      display: block;
-      transition: opacity 3s ease;
-      opacity: 1;
-    `}
-`;
-
-const LabelBlock = styled.div`
-  margin-top: 10px;
-  font-size: 24px;
-`;
-
-const Label = styled.button`
-  color: black;
-  font-size: 24px;
-
-  &:hover {
-    color: #007bff;
-  }
-`;
-
-const Table = styled.table`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: lightGray;
-`;
-
-const TableHeader = styled.thead`
-  margin: 20px 0px;
-`;
-
-const Header = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: scroll;
-  width: 100vw;
-  padding: 0 10px;
-`;
-
-const Link = styled.div`
-  margin: 30px;
-  font-weight: bold;
-
-  &:hover {
-    color: #007bff;
-  }
-
-  ${(props) =>
-    props.isUnderlined &&
-    css`
-      font-weight: 800;
-      text-decoration: underline;
-      color: red;
-    `}
-`;
+// Tab Panel Component
+const TabPanel = ({ children, value, index, ...other }) => {
+  return (
+      <Box
+          role="tabpanel"
+          hidden={value !== index}
+          id={`tabpanel-${index}`}
+          aria-labelledby={`tab-${index}`}
+          {...other}
+          sx={{
+            p: 3,
+            display: value !== index ? 'none' : 'block',
+            opacity: value !== index ? 0 : 1,
+            transition: 'opacity 0.3s ease-in-out'
+          }}
+      >
+        {value === index && children}
+      </Box>
+  );
+};
 
 export default Products;
